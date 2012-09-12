28a948ca1063df2ea6315199b68c911f235fbc42
Richard Rozeboom <hypersqual@gmail.com>
2012-09-12 16:57:33 +0200

    Value Iteration works.. maybe?

--- AA1/src/Prey.java
+++ AA1/src/Prey.java
@@ -1,6 +1,9 @@
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 
@@ -14,34 +17,67 @@ public class Prey {
 		this.pos = pos;
 	}
 	
+	public Prey(Prey prey)
+	{
+		this.pos = new Point(prey.pos);
+	}
 	
-	public void doAction(Map environment)
+	
+	public void doAction(State environment)
 	{
 		Random generator = new Random();
-		double randomNumber = generator.nextDouble();
-		if( randomNumber < 0.8 )
+		/*if( randomNumber < 0.8 )
 		{
 			//stay in the same position
 			return;
 		}else
 		{
-			//find the positions around the prey
-			List<Point> validMoves = new ArrayList<Point>();
-			validMoves.add(Map.nextTo(pos, "N"));
-			validMoves.add(Map.nextTo(pos, "E"));
-			validMoves.add(Map.nextTo(pos, "S"));
-			validMoves.add(Map.nextTo(pos, "W"));
-			
-			for(int i = 0; i < environment.agents.size(); i++){
-				if(validMoves.contains(environment.agents.get(i).pos))
-					validMoves.remove(environment.agents.get(i).pos);
-			}
+			Map<Point, > validMoves = getValidMoves(environment);
 			if(validMoves.size() == 0){
 				return;
 			}
 			this.pos = validMoves.get(generator.nextInt(validMoves.size()));
+		}*/
+		
+		Map<Point, Double> hashedMoves = getValidMoves(environment);
+		double randomDouble = generator.nextDouble();
+		double sum = 0;
+		for( Point key: hashedMoves.keySet() )
+		{
+			sum += hashedMoves.get(key);
+			if( sum > randomDouble )
+			{
+				this.pos = key;
+				return;
+			}
 		}
+			
+	}//end doAction
+	
+	public Map<Point, Double> getValidMoves(State environment)
+	{
+		//find the positions around the prey
+		List<Point> validMoves = new ArrayList<Point>();
+		//List<String> validActions = new ArrayList<String>();
+		validMoves.add(State.nextTo(pos, "N"));
+		validMoves.add(State.nextTo(pos, "E"));
+		validMoves.add(State.nextTo(pos, "S"));
+		validMoves.add(State.nextTo(pos, "W"));
+		
+		for(int i = 0; i < environment.agents.size(); i++){
+			if(validMoves.contains(environment.agents.get(i).pos))
+				validMoves.remove(environment.agents.get(i).pos);
+		}
+		Map<Point, Double> hashedMoves = new HashMap<Point, Double>();
+		hashedMoves.put(environment.prey.pos, 0.8);
+		for(int i=0; i<validMoves.size(); i++)
+		{
+			hashedMoves.put(validMoves.get(i), 0.2/validMoves.size());
+		}
+			
+		return hashedMoves;
 	}
+	
 	
 	public String toString()
 	{
