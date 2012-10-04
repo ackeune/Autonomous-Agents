fa38ac5a3dd6f67dad4ea29f4b8a27c94da89da7
Michael Cabot <michael.a.cabot@gmail.com>
2012-10-04 14:04:43 +0200

    plots with fixed gamma and different alphas

--- AA2/makePlots.m
+++ AA2/makePlots.m
@@ -1,5 +1,6 @@
 function makePlots
 
+if false
 eGreedyFixedE1I15 = averageEpisodeLengths('episodeLengths_egreedy', 0, 9);
 eGreedyFixedE1I15Subset = eGreedyFixedE1I15([1, 4, end-3, end], :);
 h = figure;
@@ -45,6 +46,42 @@ xlabel('episode');
 ylabel('episode length');
 saveas(h, '../qLearningA5G9I15', 'pdf');
 
+end
 
+eGreedyFixedE1I15 = averageEpisodeLengths('episodeLengths_egreedy', 0, 9);
+eGreedyG1 = eGreedyFixedE1I15([1, 5, 9, 13, 17], :);
+h = figure;
+plot(eGreedyG1');
+legend('a=0.1', 'a=0.2', 'a=0.3', 'a=0.4', 'a=0.5');
+xlabel('episode');
+ylabel('episode length');
+title('g=0.1')
+saveas(h, '../eGreedyFixedG1', 'pdf');
 
+eGreedyG5 = eGreedyFixedE1I15([2, 6, 10, 14, 18], :);
+h = figure;
+plot(eGreedyG5');
+legend('a=0.1', 'a=0.2', 'a=0.3', 'a=0.4', 'a=0.5');
+xlabel('episode');
+ylabel('episode length');
+title('g=0.5')
+saveas(h, '../eGreedyFixedG5', 'pdf');
+
+eGreedyG7 = eGreedyFixedE1I15([3, 7, 11, 15, 19], :);
+h = figure;
+plot(eGreedyG7');
+legend('a=0.1', 'a=0.2', 'a=0.3', 'a=0.4', 'a=0.5');
+xlabel('episode');
+ylabel('episode length');
+title('g=0.7')
+saveas(h, '../eGreedyFixedG7', 'pdf');
+
+eGreedyG9 = eGreedyFixedE1I15([4, 7, 12, 16, 20], :);
+h = figure;
+plot(eGreedyG9');
+legend('a=0.1', 'a=0.2', 'a=0.3', 'a=0.4', 'a=0.5');
+xlabel('episode');
+ylabel('episode length');
+title('g=0.9')
+saveas(h, '../eGreedyFixedG9', 'pdf');
 
